e0b625d7842165c2f49fa48eba1fc0e47fe742de
liang.hong <39-liang.hong@users.noreply.gitlab.ubixai.com>
2023-05-05 16:02:44 +0800

    feat: 添加logo动效

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -6,16 +6,19 @@ const toggleDark = useToggle(isDark)
 const openSetting = () => {
   ElMessage.warning('coming soon!')
 }
+// flex items-center justify-between w-80vw border-b border-b-solid p-t-16px p-b-16px p-l-10vw p-r-10vw fixed top-0 left-0
 </script>
 <template>
-  <div class="flex items-center justify-between w-80vw border-b border-b-solid p-t-16px p-b-16px p-l-10vw p-r-10vw fixed top-0 left-0" style="border-color: var(--border-color)">
+  <div flex="~ items-center justify-between" b="b-1px b-solid" p="t-16px b-16px l-10vw r-10vw"
+    position="fixed top-0 left-0" w="80vw" class="b-b-hb">
     <div class="flex items-center">
-      <img width="40" height="40" src="~/assets/icons/countdown.svg" />
+      <img width="40" height="40" src="~/assets/icons/countdown.svg" class="animate-duration-2000 animate-iteration-infinite animate-rotate-out" hover="animate-none"/>
       <span class="font-500 text-2xl m-l-10px">Count Down</span>
     </div>
     <div class="flex items-center">
-      <div class="text-2xl m-r-16px op-40" hover="op-80" :class="[!isDark ? 'i-solar-sun-linear' : 'i-solar-moon-broken']" @click="toggleDark()"></div>
-      <div class="i-solar-settings-linear text-2xl  op-40"  hover="op-80" @click="openSetting"></div>
+      <div class="text-2xl m-r-16px op-40" hover="op-80" :class="[!isDark ? 'i-solar-sun-linear' : 'i-solar-moon-broken']"
+        @click="toggleDark()"></div>
+      <div class="i-solar-settings-linear text-2xl  op-40" hover="op-80" @click="openSetting"></div>
     </div>
   </div>
 </template>
--- uno.config.ts
+++ uno.config.ts
@@ -14,6 +14,9 @@ export default defineConfig({
       sans: '\'Aldrich\', \'ZCOOL KuaiLe\',sans-serif',
       mono: '\'Fira Code\', monospace'
     },
+    colors: {
+      hb: 'var(--border-color)'
+    }
   },
   shortcuts: {
     responsiveBorder: 'absolute flex items-center justify-center bg-light-700 dark:bg-dark-800 [&>span]-(w-4 h-4 text-gray-400)',
