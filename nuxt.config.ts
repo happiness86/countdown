8c9fc55a61ce990d54b8077f2dcc6342ccdd66af
liang.hong <39-liang.hong@users.noreply.gitlab.ubixai.com>
2023-05-05 10:49:44 +0800

    fix: 修改element-plus配置

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@unocss/nuxt',
-    '@element-plus/nuxt'
-  ],
-  css: [
-    'element-plus/theme-chalk/dark/css-vars.css'
-  ]
-})
+  modules: ["@unocss/nuxt", "@element-plus/nuxt"],
+  elementPlus: {
+    injectionID: { prefix: 10000, current: 1 },
+  },
+  css: ["element-plus/theme-chalk/dark/css-vars.css"],
+});
