c75e59e98a43c91850acbb055ce9875a54dd68d0
liang.hong <39-liang.hong@users.noreply.gitlab.ubixai.com>
2023-05-05 10:03:12 +0800

    fix: add tips

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -3,6 +3,9 @@ import { useDark, useToggle } from '@vueuse/core'
 
 const isDark = useDark()
 const toggleDark = useToggle(isDark)
+const openSetting = () => {
+  ElMessage.warning('coming soon!')
+}
 </script>
 <template>
   <div class="flex items-center justify-between w-80vw border-b border-b-solid p-t-16px p-b-16px p-l-10vw p-r-10vw fixed top-0 left-0" style="border-color: rgba(60, 60, 67, .12)">
@@ -12,7 +15,7 @@ const toggleDark = useToggle(isDark)
     </div>
     <div class="flex items-center">
       <div class="text-2xl m-r-16px op-40" hover="op-80" :class="[!isDark ? 'i-solar-sun-linear' : 'i-solar-moon-broken']" @click="toggleDark()"></div>
-      <div class="i-solar-settings-linear text-2xl  op-40"  hover="op-80"></div>
+      <div class="i-solar-settings-linear text-2xl  op-40"  hover="op-80" @click="openSetting"></div>
     </div>
   </div>
 </template>
