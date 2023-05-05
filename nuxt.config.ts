b4c55c4c9e7ccd262d4c94b9fe215d99dfd7c56d
liang.hong <39-liang.hong@users.noreply.gitlab.ubixai.com>
2023-05-05 18:52:56 +0800

    feat: 添加nuxt  dev tools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@unocss/nuxt", "@element-plus/nuxt"],
+
   elementPlus: {
     injectionID: { prefix: 10000, current: 1 },
   },
-  css: ["element-plus/theme-chalk/dark/css-vars.css"]
+
+  css: ["element-plus/theme-chalk/dark/css-vars.css"],
+
+  devtools: {
+    enabled: true
+  }
 });
