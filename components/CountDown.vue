ad58a85e30fde9bd05339ae3154c6db232d0a555
liang.hong <39-liang.hong@users.noreply.gitlab.ubixai.com>
2023-05-05 14:26:59 +0800

    fix: 修改样式

--- components/CountDown.vue
+++ components/CountDown.vue
@@ -54,7 +54,7 @@ onUnmounted(() => {
           <div class="i-solar-clock-circle-bold-duotone text-rose text-22px m-r-8px"></div>
           <el-countdown format="HH [时] mm [分] ss [秒]" :value="knockOff" class=" text-rose"></el-countdown>
         </div>
-        <el-progress :percentage="knockOffPercentage" :stroke-width="15" striped striped-flow class="w-50vw" />
+        <el-progress :percentage="knockOffPercentage" :stroke-width="15" :duration="10" striped striped-flow class="w-50vw" />
       </div>
       <div class="m-t-48px text-20px font-500 shadow-xl m-l-16px p-32px w-80vw">
         <div class="m-b-16px">距离<span class="text-rose">{{ nextHoliday.name }}</span>放假还有：</div>
@@ -62,7 +62,7 @@ onUnmounted(() => {
           <div class="i-solar-clock-circle-bold-duotone text-rose text-22px m-r-8px"></div>
           <el-countdown format="DD [天] HH [时] mm [分] ss [秒]" :value="countdown" class=" text-rose"></el-countdown>
         </div>
-        <el-progress :percentage="countdownPercentage" :stroke-width="15" striped striped-flow class="w-50vw" />
+        <el-progress :percentage="countdownPercentage" :stroke-width="15" :duration="10" striped striped-flow class="w-50vw" />
       </div>
     </client-only>
   </div>
