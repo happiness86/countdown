9327dab1a6240d6d91ac738d813d5e7c78dd93b4
liang.hong <39-liang.hong@users.noreply.gitlab.ubixai.com>
2023-05-09 11:52:13 +0800

    fix: 配置环境变量区分vercel和gh-pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,9 @@ export default defineNuxtConfig({
   elementPlus: {
     injectionID: { prefix: 10000, current: 1 },
   },
-
+  app: {
+    baseURL: process.env.NUXT_APP_GITHUB_PAGES === 'true' ? '/countdown/' : '/'
+  },
   css: ["element-plus/theme-chalk/dark/css-vars.css"],
 
   devtools: {
