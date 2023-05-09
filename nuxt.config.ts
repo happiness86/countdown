e1fd9ab1db99d8080a8ca54f7b0dd37279258a5a
liang.hong <39-liang.hong@users.noreply.gitlab.ubixai.com>
2023-05-09 11:28:46 +0800

    fix: 修改子路径问题

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
-  app: {
-    baseURL: '/countdown/'
-  },
   modules: ["@unocss/nuxt", "@element-plus/nuxt"],
   elementPlus: {
     injectionID: { prefix: 10000, current: 1 },
